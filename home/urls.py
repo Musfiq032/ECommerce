71a33c687b4540a7c62ec5b8a917c056c46effea
Musfiq-IT <musfiq032@gmail.com>
2022-12-29 20:16:57 +0600

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from home.views import *
+from django.urls import path
+
+app_name = 'home'
+urlpatterns = [
+                  path('', home_view, name='Home'),
+                  path('', contact_view, name='Contact_Us'),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
+                                                                                         document_root=settings.STATIC_ROOT)
